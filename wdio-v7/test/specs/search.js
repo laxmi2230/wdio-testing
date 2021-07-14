2bc2433498673f26e696a32f4b71f468ac29aea6
laxmi menon <46654095+laxmi2230@users.noreply.github.com>
2021-07-14 15:47:38 +0530

    Update search.js

--- wdio-v7/test/specs/search.js
+++ wdio-v7/test/specs/search.js
@@ -1,8 +1,12 @@
+//import BasicPage from '../pages/basic.page'
+const SearchPage = require('../pages/search.page')
+
+
 describe('text works correctly', () => {
     it('should have test fitness', () => {
         browser.url('https://content.werally.com/search?topic=Fitness')
-        const infotitle = $('.has-text-wrap')
-        expect(infotitle).toHaveTextContaining('Fitness')
+    //    const infotitle = $('.has-text-wrap')
+        expect(SearchPage.infotitle).toHaveTextContaining('Fitness')
     })
 });
 
