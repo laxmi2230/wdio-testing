e142586e5d0c8134d8b26d032a4ecadbe8bfcc53
laxmi menon <46654095+laxmi2230@users.noreply.github.com>
2021-07-14 15:50:31 +0530

    Update basic.page.js

--- wdio-v7/test/pages/basic.page.js
+++ wdio-v7/test/pages/basic.page.js
@@ -10,21 +10,53 @@ class Page {
   class BasicPage extends Page {
    // const searchEl = await $('[name="cta-search"]');
     // const searchBtn = await $('[name="cta-search"]');
-  
-   get searchEl(){
-       return $('[name="cta-search"]');
+   // const category = await $('[name="cta-Food-link"]')
+   //  const headingName =  $('h2.article-title')
+   // const linkEl = $('[name="cta-article-link"]')
+   //const accountTitle =  await $('#email')
+   //const banner = $('.content-article-list-info')   
+   //  await BasicPage.searchEl.addValue('COVID-19');
+   // await BasicPage.searchBtn.click();
+   // const linkEl = await $('[name="cta-article-link"]')
+   //  const accountTitle =  await $('#email')
+   //const elem = $('.is-rds-h6')
+
+   
+
+   
+
+    get elem() {
+        return $('.is-rds-h6')
+    }
+
+   get category() {
+       return $('#QSIFeedbackButton-btn')
+    }
+
+   get headingName() {
+       return  $('h2.article-title')
    }
-  
-   get searchBtn() {
-       return $('[name="cta-search"]');
-   }
-  
+   
+   get linkEl() {
+       return $('[data-ui-element-name="cta-article-link"]')
+    }
+
+    get banner() {
+        return  $('.content-article-list-info') 
+    }
+
+    get inputEl() {
+        return $('#email')
+    }
+    
+
+   
    open() {
        super.open('http://.....');
    }
   
    
   }
+module.exports =  new BasicPage()
 
-
-  export default new BasicPage()
+  
