a9349785b526e39599ad6224423ad0b249e7b4ab
laxmi menon <46654095+laxmi2230@users.noreply.github.com>
2021-07-14 15:49:05 +0530

    Update basic.js

--- wdio-v7/test/specs/basic.js
+++ wdio-v7/test/specs/basic.js
@@ -1,4 +1,4 @@
-
+//import BasicPage from '../pages/basic.page'
 const BasicPage = require('../pages/basic.page')
 
 describe('works correctly', () => {
@@ -6,62 +6,32 @@ describe('works correctly', () => {
        browser.url('https://content.werally.com/')
       expect(browser).toHaveTitle('All Articles | Rally')
     })
-    it('should have correct class name', () => {
-       BasicPage.searchEl.addValue('health');
-       BasicPage.searchBtn.click();
-        
-       expect(BasicPage.searchEl).toHaveValue('health')
-        //await browser.pause(6000);
-    })
     it('should verify correct title name', () => {
-      //  const = $('.has-text-wrap')
-        expect(browser).toHaveTitle('Article Search Results | Rally')
+      expect(BasicPage.elem).toHaveTextContaining('Articles')
         
     });
-    it('should have correct category', async() => {
-      const category = await $('[name="cta-Food-link"]')
-      expect(category).toHaveText('Food')        
+    it('should have correct category', () => {
+      let clickable = BasicPage.category.isClickable()  
+      console.log(clickable)      
     });
 
    it('should have correct h2 name', () => {
-       const headingName =  $('h2.article-title')
-
-       expect(headingName).toHaveText('5 Summer Health Myths –– Can You Sort Fact From Fiction?')
-       
+       expect(BasicPage.headingName).toHaveText('5 Summer Health Myths –– Can You Sort Fact From Fiction?')
    });
-   it('should have correct link and is clickable', () => {
-       const linkEl = $('[name="cta-article-link"]')
-       expect(linkEl).toBeClickable()
-    });
-    it('should open correct url', async() => {
-        const linkEl = await $('[name="cta-article-link"]')
-
-
-        linkEl.click()
+    it('should open correct url', () => {
+        BasicPage.linkEl.click()
         expect(browser).toHaveUrl('https://content.werally.com/article/420380')
-
-       
-
-
-    });
+     });
 })
-
-
 describe('text works correctly', () => {
-    it('should have acoount name', async() => {
+    it('should have acoount name', () => {
         browser.url('https://accounts.werally.com/login')
-       const accountTitle =  await $('#email')
-        accountTitle.setValue('laxmi@gmail.com')
-
-    await browser.pause(6000)
+        BasicPage.inputEl.setValue('laxmi@gmail.com')
     })
 });
-
 describe('to have correct div element', () => {
     it('should render correctly', () => {
-      browser.url('https://content.werally.com/search?topic=Mental%20Health')
-      const banner = $('.content-article-list-info')   
-      expect(banner).toBeDisplayed();    
+      browser.url('https://content.werally.com/search?topic=Mental%20Health')  
+      expect(BasicPage.banner).toBeDisplayed();    
     });
-    
 });
